baea7ac1c6c94fcb0a01848ebc92c122850e4ae6
FireAssassin <radek.bienko.02@wp.pl>
2023-02-07 22:14:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
+class Countdown {
+    constructor(date, name) {
+        this.name = name;
+        this.date = date;
+        this.countdown = document.createElement("div");
+        this.countdown.classList.add("countdown");
+        this.countdown.innerHTML = `<span class="titlecd">${this.name}</span><br><span class="cd"></span>`;
+        this.title = this.countdown.querySelector("span.titlecd");
+        this.span = this.countdown.querySelector("span.cd");
+        this.interval = null;
+    }
 
+    start() {
+        if (new Date(this.date) < new Date()) {
+            throw new Error("Date is old");
+        }
+        this.interval = setInterval(() => {
+            const timeLeft = this.getTimeLeft();
+            this.span.innerHTML = `${timeLeft.months} months, ${timeLeft.weeks} weeks, ${timeLeft.days} days, ${timeLeft.hours} hours, ${timeLeft.minutes} minutes, ${timeLeft.seconds} seconds`;
+            if (timeLeft.total <= 0) {
+                this.span.innerHTML = "Done!";
+                clearInterval(this.interval);
+            }
+        }, 1000);
+    }
+
+    getTimeLeft() {
+        const total = Date.parse(this.date) - Date.parse(new Date());
+        const seconds = Math.floor((total / 1000) % 60);
+        const minutes = Math.floor((total / 1000 / 60) % 60);
+        const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+        const days = Math.floor(total / (1000 * 60 * 60 * 24) % 7);
+        const weeks = Math.floor(total / (1000 * 60 * 60 * 24 * 7) % 4);
+        const months = Math.floor(total / (1000 * 60 * 60 * 24 * 7 * 4));
+        return {
+            total,
+            months,
+            weeks,
+            days,
+            hours,
+            minutes,
+            seconds,
+        };
+    }
+}
+
+fetch('https://fireassassin848.github.io/c0untdownTo/dates.json')
+    .then(response => response.json())
+    .then(json => {
+        for (i = 0; i < 12; i++) {
+
+            let date = json[i]['month']+" "+json[i]['day']+" "+new Date().getFullYear()
+
+            if (new Date(date) > new Date()) {
+                const countdown = new Countdown(date, json[i]['name']);
+                countdown.start();
+                document.body.appendChild(countdown.countdown);
+            }
+        }
+    })
